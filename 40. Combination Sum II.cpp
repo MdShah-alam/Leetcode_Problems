bfa75c24d998a88060f98207b8fdbf3c5568b8a8
Md .  Shah Alam <94603993+MdShah-alam@users.noreply.github.com>
2024-07-10 12:26:56 +0600

    Add files via upload

--- 40. Combination Sum II.cpp
+++ 40. Combination Sum II.cpp
@@ -1,29 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void findcombination(int ind , int target , vector<int>&ar , vector<vector<int>>&ans , vector<int>&v)
+void findCombination(int ind,int t,vector<int>&v,vector<vector<int>>&ans,vector<int>&ds)
 {
-    if(ind == ar.size()){
-        if(target==0)
-            ans.push_back(v);
+    if(t==0){
+        ans.push_back(ds);
         return ;
     }
-
-    // pick up the element
-    if(ar[ind]<=target){
-        v.push_back(ar[ind]);
-        findcombination(ind+1, target-ar[ind],ar , ans , v);
-        v.pop_back();
+    for(int i=ind;i<v.size();i++){
+        if(i>ind && v[i]==v[i-1]) continue;
+        if(v[i]>t)break;
+        ds.push_back(v[i]);
+        findCombination(i+1,t-v[i],v,ans,ds);
+        ds.pop_back();
     }
-
-    findcombination(ind+1 , target , ar , ans , v);
 }
 
-vector<vector<int>> combinationSum2(vector<int>& candidates, int target)
+vector<vector<int>>combinationSum2(vector<int>& v, int t)
 {
+    sort(v.begin(),v.end());
     vector<vector<int>>ans;
-    vector<int>v;
-    findcombination(0,target , candidates , ans , v);
+    vector<int>ds;
+    findCombination(0,t,v,ans,ds);
     return ans;
 }
 
@@ -31,20 +29,17 @@ int main()
 {
     int n;
     cin>>n;
-    vector<int>v;
-    for(int i=0;i<n;i++){
-        int a;
-        cin>>a;
-        v.push_back(a);
-    }
+    vector<int>v(n);
+    for(int i=0;i<n;i++)
+        cin>>v[i];
     int t;
     cin>>t;
-    vector<vector<int>>vv = combinationSum2(v,t);
+
+    vector<vector<int>>vv=combinationSum2(v,t);
 
     for(int i=0;i<vv.size();i++){
-        for(int j=0;j<vv[i].size();j++){
+        for(int j=0;j<vv[i].size();j++)
             cout<<vv[i][j]<<" ";
-        }
         cout<<endl;
     }
     return 0;
