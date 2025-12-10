cdf681724e8341d8c7d51a1c85d59119c49f3048
Md .  Shah Alam <94603993+MdShah-alam@users.noreply.github.com>
2025-12-10 18:34:49 +0600

    Add files via upload

--- 134. Gas Station.cpp
+++ 134. Gas Station.cpp
@@ -9,11 +9,11 @@ int canCompleteCircuit(vector<int>& gas, vector<int>& cost)
         total_tank+=diff;
         curr_tank+=diff;
         if(curr_tank<0){
-            curr_tank=0;
             start_station=i+1;
+            curr_tank=0;
         }
     }
-    return total_tank>=0 ? start_station : -1;
+    return total_tank >=0 ? start_station : -1;
 }
 
 int main()
@@ -21,21 +21,10 @@ int main()
     int n;
     cin>>n;
     vector<int>gas(n),cost(n);
-
     for(int i=0;i<n;i++)
         cin>>gas[i];
-
     for(int i=0;i<n;i++)
         cin>>cost[i];
-
     cout<<canCompleteCircuit(gas,cost)<<endl;
     return 0;
 }
-
-/**
-
-5
-1 2 3 4 5
-3 4 5 1 2
-
-*/
